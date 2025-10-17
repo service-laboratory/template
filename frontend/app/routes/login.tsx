25d1f84ef43905fbd9a441edf3a978fbadb9a9db
rysev-a <rysev-a@yandex.ru>
2025-10-17 19:06:51 +0300

    Use auth package

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -1,13 +1,43 @@
 import { LoginForm } from "@service_laboratory/auth";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router";
 
 export function meta() {
   return [{ title: "Login" }];
 }
 
 export default function LoginPage() {
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
-    <h1>
-      <LoginForm />
-    </h1>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm
+          form={form}
+          submit={(values) => {
+            console.log(values);
+          }}
+          loginFormTitle="Login"
+          loginFormDescription="Please enter your email and password to login."
+          emailLabel="Email"
+          passwordLabel="Password"
+          forgotPasswordMessage="Forgot password?"
+          submitButtonLabel="Login"
+          resetPasswordLinkComponent={() => (
+            <Link
+              className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+              to={"/reset-password"}
+            >
+              forgot password?
+            </Link>
+          )}
+        />
+      </div>
+    </div>
   );
 }
