d22504a562a502ab06fcc154500cdedd8b38defb
Alexey Rysev <rysev-a@yandex.ru>
2025-10-24 12:31:23 +0300

    Use openapi from lib

---
+++ packages/pypi/service_laboratory/src/core/openapi.py
@@ -0,0 +1,27 @@
+from litestar.openapi import OpenAPIConfig
+from litestar.openapi.plugins import ScalarRenderPlugin
+from litestar.openapi.spec import Components, SecurityScheme
+
+from .settings import settings
+
+
+openapi_config = OpenAPIConfig(
+    components=[
+        Components(
+            security_schemes={
+                "JWT": SecurityScheme(
+                    type="http",
+                    scheme="Bearer",
+                    name="Authorization",
+                    security_scheme_in="cookie",
+                    bearer_format="JWT",
+                    description="Authorization",
+                )
+            }
+        )
+    ],
+    title=settings.open_api_config.title,
+    version=settings.open_api_config.version,
+    path=settings.open_api_config.path,
+    render_plugins=[ScalarRenderPlugin()],
+)
--- packages/pypi/service_laboratory/src/core/settings.py
+++ packages/pypi/service_laboratory/src/core/settings.py
@@ -2,8 +2,18 @@ from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
+class OpenApiSettings(BaseSettings):
+    title: str = Field(deafult="title")
+    version: str = Field(deafult="0.1.2")
+    path: str = Field(deafult="/api/docs")
+
+    class Config:
+        env_prefix = "OPEN_API_"
+
+
 class Settings(BaseSettings):
     db_url: str = Field(default="postgresql+asyncpg://user:password@localhost:5432/db")
+    open_api_config: OpenApiSettings = Field(default_factory=OpenApiSettings)
 
 
 settings = Settings()
